02da4f463c88d875ae070656ff5de1a92010fde3
Polina Koval <kovalp@yandex-team.ru>
2017-05-16 10:05:28 +0500

    env2048

--- my2048-rl-theano-n-tuple-Copy7.py
+++ my2048-rl-theano-n-tuple-Copy7.py
@@ -1,18 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-from c2048 import Game, push
-
-# In[4]:
+from env2048.env2048 import Game2048
+from c2048 import push
 
 import numpy as np
 import theano
 import theano.tensor as T
 import lasagne
 from lasagne.layers import DenseLayer, InputLayer
-from lasagne.layers import MergeLayer, ReshapeLayer, FlattenLayer, ConcatLayer
+from lasagne.layers import FlattenLayer, ConcatLayer
 
-floatX = theano.config.floatX
 floatX = theano.config.floatX
 
 from lasagne.layers.dnn import Conv2DDNNLayer
@@ -46,27 +41,18 @@ l_out = DenseLayer(_, num_units=1, nonlinearity=None)
 prediction = lasagne.layers.get_output(l_out)
 P = theano.function([input_var], prediction)
 loss = lasagne.objectives.squared_error(prediction, target_var).mean() / 2
-# layers = {conv1: 0.5, conv2: 0.5}
-# l1_penalty = regularize_layer_params_weighted(layers, l1)
-# loss = loss + 1e-4 * l1_penalty
 accuracy = lasagne.objectives.squared_error(prediction, target_var).mean()
 params = lasagne.layers.get_all_params(l_out, trainable=True)
-# params = [l_out.W]
 updates = lasagne.updates.adam(loss, params, beta1=0.5)
-# updates = lasagne.updates.adamax(loss, params)
 
 
 train_fn = theano.function([input_var, target_var], loss, updates=updates)
 loss_fn = theano.function([input_var, target_var], loss)
 accuracy_fn = theano.function([input_var, target_var], accuracy)
 
-# In[9]:
-
-from random import randint
 
 table = {2 ** i: i for i in range(1, 16)}
 table[0] = 0
-
 
 def make_input(grid):
 	g0 = grid
@@ -76,31 +62,6 @@ def make_input(grid):
 			v = g0[i, j]
 			r[table[v], i, j] = 1
 	return r
-
-
-logf = open("logf-rl-theano-n-tuple-6", "w")
-
-
-def get_grid(driver):
-	grid = np.zeros(shape=(4, 4), dtype='uint16')
-	for x in driver.find_elements_by_class_name('tile'):
-		cl = x.get_attribute('class').split()
-		for t in cl:
-			if t.startswith('tile-position-'):
-				pos = int(t[14]) - 1, int(t[16]) - 1
-			elif t.startswith('tile-') and t[5].isdigit():
-				v = int(t[5:])
-		grid[pos[1], pos[0]] = v
-	return grid
-
-
-# import matplotlib
-# import matplotlib.pyplot as plt
-#
-# matplotlib.rcParams['figure.figsize'] = (12.0, 8.0)
-
-import time
-
 
 def Vchange(grid, v):
 	g0 = grid
@@ -115,32 +76,20 @@ def Vchange(grid, v):
 	ytrain = np.array([v] * 8, dtype=floatX)
 	train_fn(xtrain, ytrain)
 
+arrow = [3, 0, 1, 2]
 
-arrow = [Keys.ARROW_LEFT, Keys.ARROW_UP, Keys.ARROW_RIGHT, Keys.ARROW_DOWN]
-
-
-def gen_sample_and_learn(driver):
-	body = driver.find_element_by_tag_name('body')
+def gen_sample_and_learn(game):
 	game_len = 0
 	game_score = 0
 	last_grid = None
-	keep_playing = False
 	while True:
-		try:
-			grid_array = get_grid(driver)
-		except:
-			grid_array = None
+		grid_array = game.grid.get_values()
 		board_list = []
-		if grid_array is not None:
-			if not keep_playing and grid_array.max() == 2048:
-				driver.find_element_by_class_name('keep-playing-button').click()
-				keep_playing = True
-				time.sleep(1)
-			for m in range(4):
-				g = grid_array.copy()
-				s = push(g, m % 4)
-				if s >= 0:
-					board_list.append((g, m, s))
+		for m in range(4):
+			g = grid_array.copy()
+			s = push(g, m % 4)
+			if s >= 0:
+				board_list.append((g, m, s))
 		if board_list:
 			boards = np.array([make_input(g) for g, m, s in board_list], dtype=floatX)
 			p = P(boards).flatten()
@@ -154,7 +103,7 @@ def gen_sample_and_learn(driver):
 					best_move = m
 					best_score = 2 * s
 					best_grid = boards[i]
-			body.send_keys(arrow[best_move])
+			game.step(arrow[best_move])
 			game_score += best_score
 		else:
 			best_v = 0
@@ -164,36 +113,16 @@ def gen_sample_and_learn(driver):
 		last_grid = best_grid
 		if not board_list:
 			break
-		# plt.pause(0.05)
 	return game_len, grid_array.max(), game_score
 
 
 results = []
-driver = webdriver.Firefox()
-# graph = plt.plot([], [], 'b')[0]
-# dots256 = plt.plot([], [], 'ro')[0]
-# dots512 = plt.plot([], [], 'yo')[0]
-# dots1024 = plt.plot([], [], 'go')[0]
-# plt.xlim((0, 100))
-# plt.ylim((0, 25000))
+game = Game2048(4)
 for j in range(200):
-	driver.get("https://gabrielecirulli.github.io/2048/")
-	time.sleep(2)
-	result = gen_sample_and_learn(driver)
+	game.reset()
+	result = gen_sample_and_learn(game)
 	print(j, result)
 	results.append(result)
-	# graph.set_data(np.arange(len(results)), np.array(results)[:, 2])
 	dots_data = [[], [], []]
-	for i, d in enumerate(results):
-		c = 0 if d[1] <= 256 else (1 if d[1] == 512 else 2)
-		dots_data[c].append([i, d[2]])
-	# dots_graph = [dots256, dots512, dots1024]
-	# for i in range(3):
-	# 	if dots_data[i]:
-	# 		xy = np.array(dots_data[i])
-			# dots_graph[i].set_data(xy[:, 0], xy[:, 1])
-	# plt.title("Game #%d" % j, fontsize=64)
-	# plt.draw()
-	# plt.pause(3)
 	if result[1] >= 2048:
 		break
